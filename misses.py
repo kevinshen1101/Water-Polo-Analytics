3d646a33d7c1f57dd514fa751bf4108a53cb87c7
ldnovak <ldnovak@mit.edu>
2019-05-15 00:47:39 -0400

    found mistake with turnovers

--- misses.py
+++ misses.py
@@ -4,6 +4,8 @@ import pandas as pd
 def get_miss_breakdown(df):
     goalie_block = sum(df['Block'] == 'goalie')
     field_block = sum(df['Block'] == 'field')
+    turn_overs = sum(df['Turnover'] == 1)
     non_goal = sum(df['Goal'] == 0)
-    return {'goalie': goalie_block, 'field': field_block, 'missed': non_goal - goalie_block - field_block}
+    return {'goalie': goalie_block, 'field': field_block,
+            'missed': non_goal - goalie_block - field_block - turn_overs, "turnovers": turn_overs}
 
