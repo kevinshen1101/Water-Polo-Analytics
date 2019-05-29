4414430460bb5e20be886577f8e93be40dbff80e
ldnovak <ldnovak@mit.edu>
2019-05-28 23:54:50 -0400

    updated get_misses to show percents

--- misses.py
+++ misses.py
@@ -7,4 +7,6 @@ def get_miss_breakdown(df):
     turn_overs = sum(df['Turnover'] == 1)
     non_goal = sum(df['Goal'] == 0)
     return {'goalie': goalie_block, 'field': field_block,
-            'missed': non_goal - goalie_block - field_block - turn_overs, "turnovers": turn_overs}
+            'missed': non_goal - goalie_block - field_block - turn_overs, "turnovers": turn_overs,
+            'percent_goalie': goalie_block/non_goal, 'percent_field': field_block/non_goal,
+            'percent_missed': (non_goal - goalie_block - field_block - turn_overs)/non_goal}
